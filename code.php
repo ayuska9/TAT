bf805a72a1f5789c2c62d1b5e952faf010bcf9e7
Ananta <107178397+Ananta0211@users.noreply.github.com>
2023-05-14 09:19:12 +0545

    Add files via upload
    this is the fixed code for the issue that had arise

--- code.php
+++ code.php
@@ -8,7 +8,9 @@ if(isset($_POST['save_package']))
     $tour_location=$_POST['tour_location'];
     $cost=$_POST['cost'];
     $image=$_FILES['image']['name'];
-    $description=$_POST['description'];
+    $description = $_POST['description'];
+$description = mysqli_real_escape_string($conn, $description); // escape single quotes
+
     $tour_status=$_POST['tour_status'];
     $date_added=date("Y-m-d H:i:s");
     $date_updated=date("Y-m-d H:i:s");
@@ -34,36 +36,39 @@ if(isset($_POST['save_package']))
             header('location: create.php');
             exit;
         } else {
-            $query="INSERT INTO packages (title, tour_location, cost, image, description, status, date_created) 
-            VALUES ('$tour_name', '$tour_location', '$cost', '$image', '$description', '$status', '$date_added')";
+            $query = "INSERT INTO packages (title, tour_location, cost, image, description, status, date_created) 
+          VALUES ('$tour_name', '$tour_location', '$cost', '$image', '$description', '$status', '$date_added')";
+
             $query_run=mysqli_query($conn,$query);
 
             if($query_run) {
                 move_uploaded_file($_FILES["image"]["tmp_name"],"package/".$_FILES["image"]["name"]);
 
                 $_SESSION['status']="Package added successfully";
-                header("location: create.php");
+                header("location: index.php");
                 exit;
             } else {
                 $_SESSION['status']="Unable to add package";
-                header("location: create.php");
+                header("location: index.php");
                 exit;
             }
         }
     }
 }
 
+
 if(isset($_POST['update_package'])){
+    $id = $_POST['package_id'];
     $tour_name=$_POST['tour_name'];
     $tour_location=$_POST['tour_location'];
     $cost=$_POST['cost'];
-    $image=$_FILES['image']['name'];
-    $new_image = $_FILES['image']['name'];
+    $image=$_POST['image'];
+    $new_image = $_FILES['new_image']['name'];
 
     if($new_image!=''){
-        $update_filename=$_FILES['image']['name'];
+        $update_filename=$_FILES['new_image']['name'];
     }else{
-        $update_filename=$_POST['old_image'];
+        $update_filename=$image;
     }
 
     $description=$_POST['description'];
@@ -77,38 +82,43 @@ if(isset($_POST['update_package'])){
         $status = 0;
     }
 
-    if ($_FILES['image']['name']!='') {
-        if (file_exists("package/" . $_FILES['image']['name'])) {
-            $_SESSION['status'] = "Image already exists: " . $_FILES['image']['name'];
+    if ($_FILES['new_image']['name']!='') {
+        if (file_exists("package/" . $_FILES['new_image']['name'])) {
+            $_SESSION['status'] = "Image already exists: " . $_FILES['new_image']['name'];
             header('location: index.php');
             exit;
         }
     }
 
-    $id = $_POST['package_id'];
-    $query="UPDATE packages SET title='$tour_name',tour_location = '$tour_location',cost= '$cost',image = '$update_filename',description = '$description',status='$status', date_updated = NOW() WHERE package_id=$id";
+    
+    $query="UPDATE packages SET title='$tour_name',tour_location = '$tour_location',cost= '$cost',image = '$update_filename',description = '$description',status='$status', date_updated = NOW() WHERE package_id='$id'";
     $query_run = mysqli_query($conn,$query);
+    
 
     if($query_run){
-        if($_FILES['image']['name']!=''){
-            move_uploaded_file($_FILES["image"]["tmp_name"],"package/".$update_filename);
-
-if($_FILES['image']['name']!='' && $image != $update_filename){
-unlink("package/".$image);
+        if($_FILES['new_image']['name']!=''){
+            move_uploaded_file($_FILES["new_image"]["tmp_name"],"package/".$update_filename);
+    
+            if($image!='' && $image != $update_filename){
+                unlink("package/".$image);
+            }
+    
+            $_SESSION['status']="Package not updated .";
+            header('location: index.php');
+            exit;
+    
+        } else {
+            $_SESSION['status']= "Package updated successfully.";
+            header('location: index.php');
+            exit;
+        }
+    }
+    
 }
 
-$_SESSION['status']="Package updated successfully.";
-header('location: index.php');
-exit;
 
-} else {
-$_SESSION['status']= "Package not updated.";
-header('location: index.php');
-exit;
-}
 
-}
-}
+
 
 if(isset($_POST['delete_package'])){
     $id=$_POST['delet_package_id'];
@@ -126,5 +136,6 @@ if(isset($_POST['delete_package'])){
         header('location: index.php');
     }
 }
+
 
 ?>
